2bf6fb7ab429ebcfaf26e0130981b9b65fe995a5
Justin Makeig <jmakeig@marklogic.com>
2017-04-17 22:27:03 -0700

    Includes polyfill for Symbols (and others)

--- src/test.js
+++ src/test.js
@@ -1,7 +1,9 @@
 'use strict';
+import 'babel-polyfill';
 import deepEqual from 'deep-equal';
 import StackTrace from 'stack-trace';
+import format from 'pretty-format';
 
 export default function() {
-  return deepEqual({ one: 1 }, { one: 1 });
+  return format({ one: 1 });
 }
